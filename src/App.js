9b30c99d7dfad7df9a5c2b6e61f609e87e3590c4
abu sayeed <sayeed0209@gmail.com>
2021-07-05 13:25:36 +0200

    fetching data through axiox & passing data to the giflist component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SearchForm from "./components/SearchForm";
+import GifList from "./components/GifList";
+import "./App.css";
+// obAPoDQILtaB9uXHZrWUZRq4XupFSWg3
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([]); // declare state
+	const [query, setQuery] = useState("cats");
+	const [isLoading, setIsLoading] = useState(true);
+	const onSearch = value => {
+		setQuery(value);
+	};
+	useEffect(() => {
+		axios(
+			`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=obAPoDQILtaB9uXHZrWUZRq4XupFSWg3`
+		)
+			.then(response => setData(response.data.data))
+			.catch(error => console.log("Error fetching and parsing data", error))
+			.finally(() => setIsLoading(false));
+	}, [query]);
+	return (
+		<>
+			<div className='main-header'>
+				<div className='inner'>
+					<h1 className='main-title'>GifSearch</h1>
+					<SearchForm onSearch={onSearch} />
+				</div>
+			</div>
+			<div className='main-content'>
+				{isLoading ? <p>Loading...</p> : <GifList data={data} />}
+			</div>
+		</>
+	);
 }
 
 export default App;
