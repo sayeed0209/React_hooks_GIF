5509f2937933bd1f37a210c32e568dfea0375756
abu sayeed <sayeed0209@gmail.com>
2021-07-05 13:24:34 +0200

    gitlist componenet

---
+++ src/components/GifList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Gif from "./Gif";
+import NoGifs from "./NoGifs";
+const GifList = props => {
+	const results = props.data;
+	let gifs;
+	if (results.length) {
+		gifs = results.map(gif => (
+			<Gif url={gif.images.fixed_height.url} key={gif.id} />
+		));
+	} else {
+		gifs = <NoGifs />;
+	}
+
+	return <ul className='gif-list'>{gifs}</ul>;
+};
+export default GifList;
